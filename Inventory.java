0f3afc07b839dc29b948ab7a73d2a4490b81d28a
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-04-29 23:08:45 -0400

    adds main panel
    NOTES:
    has the required basic elements though no logic has been implemented

--- Inventory.java
+++ Inventory.java
@@ -1,4 +1,14 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.Font;
 
 public class Inventory
 {
@@ -6,6 +16,161 @@ public class Inventory
   public static void main (String args[])
   {
     JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+    JPanel headPanel = new JPanel();
+    JLabel headLabel = new JLabel("PURCHASE ORDER");
+    headLabel.setFont(new Font("Arial", Font.BOLD, 64));
+    headPanel.add(headLabel);
+
+    JPanel formPanel = new JPanel(new GridBagLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.anchor = GridBagConstraints.WEST;
+    gbc.insets = new Insets(32, 32, 32, 32);
+
+    JLabel clientLabel = new JLabel("Client:");
+    clientLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+    Dimension dim = clientLabel.getPreferredSize();
+    dim.setSize(dim.getWidth(), 48);
+    clientLabel.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.gridwidth = 2;
+    formPanel.add(clientLabel, gbc);
+
+    JTextField clientTxt = new JTextField(64);
+    Dimension clientDim = clientTxt.getPreferredSize();
+    clientDim.setSize(clientDim.getWidth(), 48);
+    clientTxt.setPreferredSize(new Dimension(clientDim));
+
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    gbc.gridwidth = 1;
+    formPanel.add(clientTxt, gbc);
+
+    JLabel productLabel = new JLabel("Product");
+    productLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    formPanel.add(productLabel, gbc);
+
+    JLabel unitValueLabel = new JLabel("Unit Value");
+    unitValueLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+    gbc.gridx = 1;
+    gbc.gridy = 1;
+    formPanel.add(unitValueLabel, gbc);
+
+    JLabel quantityLabel = new JLabel("Quantity");
+    quantityLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+    gbc.gridx = 2;
+    gbc.gridy = 1;
+    formPanel.add(quantityLabel, gbc);
+
+    JLabel totalLabel = new JLabel("Total");
+    totalLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+    gbc.gridx = 3;
+    gbc.gridy = 1;
+    formPanel.add(totalLabel, gbc);
+
+    JTextField prodTxt1 = new JTextField(32);
+    prodTxt1.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 0;
+    gbc.gridy = 2;
+    formPanel.add(prodTxt1, gbc);
+
+    JTextField unitValTxt1 = new JTextField(32);
+    unitValTxt1.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 1;
+    gbc.gridy = 2;
+    formPanel.add(unitValTxt1, gbc);
+
+    JTextField quantityTxt1 = new JTextField(32);
+    quantityTxt1.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 2;
+    gbc.gridy = 2;
+    formPanel.add(quantityTxt1, gbc);
+
+    JTextField totalTxt1 = new JTextField(32);
+    totalTxt1.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 3;
+    gbc.gridy = 2;
+    formPanel.add(totalTxt1, gbc);
+
+
+    JTextField prodTxt2 = new JTextField(32);
+    prodTxt2.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 0;
+    gbc.gridy = 3;
+    formPanel.add(prodTxt2, gbc);
+
+    JTextField unitValTxt2 = new JTextField(32);
+    unitValTxt2.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 1;
+    gbc.gridy = 3;
+    formPanel.add(unitValTxt2, gbc);
+
+    JTextField quantityTxt2 = new JTextField(32);
+    quantityTxt2.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 2;
+    gbc.gridy = 3;
+    formPanel.add(quantityTxt2, gbc);
+
+    JTextField totalTxt2 = new JTextField(32);
+    totalTxt2.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 3;
+    gbc.gridy = 3;
+    formPanel.add(totalTxt2, gbc);
+
+
+    JTextField prodTxt3 = new JTextField(32);
+    prodTxt3.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 0;
+    gbc.gridy = 4;
+    formPanel.add(prodTxt3, gbc);
+
+    JTextField unitValTxt3 = new JTextField(32);
+    unitValTxt3.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 1;
+    gbc.gridy = 4;
+    formPanel.add(unitValTxt3, gbc);
+
+    JTextField quantityTxt3 = new JTextField(32);
+    quantityTxt3.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 2;
+    gbc.gridy = 4;
+    formPanel.add(quantityTxt3, gbc);
+
+    JTextField totalTxt3 = new JTextField(32);
+    totalTxt3.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 3;
+    gbc.gridy = 4;
+    formPanel.add(totalTxt3, gbc);
+
+
+    JLabel grandTotalLabel = new JLabel("Grand Total");
+    grandTotalLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+    gbc.gridx = 2;
+    gbc.gridy = 5;
+    formPanel.add(grandTotalLabel, gbc);
+
+    JTextField grandTotalTxt = new JTextField(32);
+    grandTotalTxt.setPreferredSize(new Dimension(dim));
+    gbc.gridx = 3;
+    gbc.gridy = 5;
+    formPanel.add(grandTotalTxt, gbc);
+
+
+    JButton button = new JButton("Calculate");
+    button.setFont(new Font("Arial", Font.PLAIN, 40));
+    gbc.gridx = 3;
+    gbc.gridy = 6;
+    formPanel.add(button, gbc);
+
+
+    panel.add(headPanel);
+    panel.add(formPanel);
+    frame.add(panel);
     frame.setTitle("Inventory");
     frame.setSize(Inventory.WINDOW_LEN, Inventory.WINDOW_LEN);
     frame.setLocationRelativeTo(null);
